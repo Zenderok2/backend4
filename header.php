1dd992bf57cfb884217b6aec33e0ad89a442a3e0
Bogdanov Rodion <166429208+Zenderok2@users.noreply.github.com>
2025-05-30 10:47:20 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Мой сайт</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <h1>Добро пожаловать</h1>
+    <nav>
+        <a href="index.php">Главная</a>
+        <a href="login.php">Вход</a>
+        <a href="admin.php">Админка</a>
+    </nav>
+</header>
+<main>
